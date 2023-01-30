c3c20ccedc3a6a73c35e97e5be6e7a26126613ae
Prachideshwal <83039452+Prachideshwal@users.noreply.github.com>
2023-01-30 19:38:15 +0530

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+// const options = {
+//   method: 'GET',
+//   url: 'https://youtube-v31.p.rapidapi.com/captions',
+//   params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
+//   headers: {
+//     'X-RapidAPI-Key': '11dfc74a20msh457989be18bedf3p1cca94jsn7a3f701e67c4',
+//     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+//   }
+// };
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
